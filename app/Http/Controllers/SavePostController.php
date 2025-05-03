562538c55abc05e305195e1fa8e1b8ab81801fc9
abdellah <abdo.abdell.2000@gmail.com>
2025-05-03 22:39:43 +0100

    premier commit pour créer branch develop et creation de quelque models pour la commit du branch develop

---
+++ app/Http/Controllers/SavePostController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SavePost;
+use App\Http\Requests\StoreSavePostRequest;
+use App\Http\Requests\UpdateSavePostRequest;
+
+class SavePostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSavePostRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SavePost $savePost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SavePost $savePost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSavePostRequest $request, SavePost $savePost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SavePost $savePost)
+    {
+        //
+    }
+}
