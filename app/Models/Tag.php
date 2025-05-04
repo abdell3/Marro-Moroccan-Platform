347ce5f444db87a1b2103ebd096f0791c16315e0
abdellah <abdo.abdell.2000@gmail.com>
2025-05-04 11:48:40 +0100

    implementer les models : User, Thread, Tag, SavePost

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
