407df90cd6fd1be35b3575d23ea4edac0211cd8b
abdellah <abdo.abdell.2000@gmail.com>
2025-05-04 12:42:14 +0100

    Implementer les models: Post, Report, ReportType, Role avec leurs fillables et relations

--- app/Models/ReportType.php
+++ app/Models/ReportType.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Report;
 
 class ReportType extends Model
 {
     /** @use HasFactory<\Database\Factories\ReportTypeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function reports()
+    {
+        return $this->hasMany(Report::class, 'type_report_id');
+    }
+    
 }
