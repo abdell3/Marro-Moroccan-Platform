a774a9c0faf5a426959d9f9160b41c76d806e85b
abdellah <abdo.abdell.2000@gmail.com>
2025-05-04 13:50:52 +0100

    implementer les models: Poll, Permission, Community, Comment, Badge avec les fillables et relations between models

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Role;
 
 class Permission extends Model
 {
     /** @use HasFactory<\Database\Factories\PermissionFactory> */
     use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_permission');
+    }
 }
