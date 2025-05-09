7684c27c517be67a380be63f301ea692323f1b0b
abdellah <abdo.abdell.2000@gmail.com>
2025-05-09 15:23:37 +0100

    fixer le probleme de reply où la methode authorize ne fonctionne pas car j'ai pas utiliser AutoriseRequest

--- app/Http/Requests/ReplyCommentRequest.php
+++ app/Http/Requests/ReplyCommentRequest.php
@@ -1,24 +1,16 @@
 <?php
 
-namespace App\Http\Requests\Comment;
+namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
 class ReplyCommentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
@@ -26,11 +18,6 @@ class ReplyCommentRequest extends FormRequest
         ];
     }
 
-    /**
-     * Get the error messages for the defined validation rules.
-     *
-     * @return array<string, string>
-     */
     public function messages(): array
     {
         return [
